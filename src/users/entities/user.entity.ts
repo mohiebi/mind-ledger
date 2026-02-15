4bac0be6c99dea64fa3c415bb7958a8dbd8fa7ae
Mohi <e.mohamadhosein@gmail.com>
2026-02-15 12:35:39 +0330

    adding user resource

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password_hash: string;
+
+  @Column()
+  name: string;
+
+  @Column({ default: 'user' })
+  role: 'user' | 'admin';
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
