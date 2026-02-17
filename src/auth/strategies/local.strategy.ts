027cf59f653edf952844d8f5ef939a52ee605b8f
Mohi <e.mohamadhosein@gmail.com>
2026-02-17 12:01:29 +0330

    creating local strategy

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,26 @@
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from 'src/user/entities/user.entity';
+import { LoginDto } from '../login.dto';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly authService: AuthService) {
+        super({
+            usernameField: 'email',
+            passwordField: 'password',
+        });
+    }
+
+    public async validate(loginDto: LoginDto): Promise<User> {
+        const user = await this.authService.validateUser(loginDto);
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid email or password');
+        }
+
+        return user;
+    }
+}
