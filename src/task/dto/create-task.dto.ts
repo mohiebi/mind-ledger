80f6d64af5ca3ce4b9322500eadacd30e8ba2a37
Mohi <e.mohamadhosein@gmail.com>
2026-02-18 12:42:21 +0330

    create task dto

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTaskDto {}
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { TaskStatus } from '../entities/task.model';
+
+export class CreateTaskDto {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsString()
+    description: string;
+
+    @IsNotEmpty()
+    @IsEnum(TaskStatus)
+    status: TaskStatus;
+}
